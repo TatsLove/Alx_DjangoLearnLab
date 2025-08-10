d742faafd7be6fd0694b647c33d64c844883cda5
Tatenda Loverage Tirivavi <tloverage@gmail.com>
2025-08-10 18:21:14 +0200

    Setup Django project with custom serializers for Author and Book models

--- advanced_features_and_security/bookshelf/admin.py
+++ advanced_features_and_security/bookshelf/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, CustomUser  # 👈 Import both models here
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
-    search_fields = ('title', 'author')
-    list_filter = ('publication_year',)
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'username', 'is_active', 'is_staff')
