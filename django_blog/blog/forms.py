7fc5ece201f56b9d8044bd73c54b33f85d9096ca
Tatenda Loverage Tirivavi <tloverage@gmail.com>
2025-08-24 15:24:11 +0200

    Fix authentication system: static files, URLs, profile update, CSRF

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,7 +1,7 @@
 from django import forms
-from .models import Post
+from django.contrib.auth.models import User
 
-class PostForm(forms.ModelForm):
+class ProfileUpdateForm(forms.ModelForm):
     class Meta:
-        model = Post
-        fields = ['title', 'content']
+        model = User
+        fields = ["username", "email"]
--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,16 +1,10 @@
 from django.urls import path
-from .views import (
-    PostListView,
-    PostDetailView,
-    PostCreateView,
-    PostUpdateView,
-    PostDeleteView
-)
+from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='post-list'),
-    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('posts/new/', PostCreateView.as_view(), name='post-create'),
-    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
-    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path("register/", views.register, name="register"),
+    path("profile/", views.profile, name="profile"),
+    path("login/", auth_views.LoginView.as_view(template_name="blog/login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(template_name="blog/logout.html"), name="logout"),
 ]
